d73cd44a58fcd060a1519bb9809f4daebd6bf385
wahyusoft <wahyusoft@yahoo.com>
2017-04-06 02:49:33 +0700

    adding update functional

--- module/category/form.php
+++ module/category/form.php
@@ -1,4 +1,17 @@
  <!-- Content Header (Page header) -->
+ <?php
+ 	if(isset($_GET['act'])=="edit"){
+ 		$stmt = $conn->prepare("SELECT * FROM categories WHERE categoryID=".$_GET["ID"]);
+		$stmt->execute();
+		$rows = $stmt->fetchAll(PDO::FETCH_OBJ);	
+ 		$CategoryName = $rows[0]->CategoryName;
+ 		$Description  = $rows[0]->Description; 		
+ 	}else{
+ 		$CategoryName = "";
+ 		$Description  = "";
+
+ 	}
+ ?>
     <section class="content-header">
       <h1>
         Categories
@@ -21,11 +34,11 @@
               <div class="box-body">
                 <div class="form-group">
                   <label for="CategoryName">Category Name</label>
-                  <input type="text" class="form-control" name="CategoryName" placeholder="Enter Category Name" required="required">
+                  <input type="text" class="form-control" name="CategoryName" placeholder="Enter Category Name" required="required" value="<?php echo $CategoryName;?>">
                 </div>
                 <div class="form-group">
                   <label for="Description">Description</label>
-                  <input type="text" class="form-control" name="Description" placeholder="Enter Description">
+                  <input type="text" class="form-control" name="Description" placeholder="Enter Description" value="<?php echo $Description;?>">
                 </div>
                 
               </div>
@@ -55,13 +68,17 @@
 			exit();
 		}
 		try {
-			$sql = "INSERT INTO categories (CategoryName, Description)VALUES ('$a','$b')";
+			if(isset($_GET['act'])=="edit")
+				$sql = "UPDATE categories SET CategoryName='$a', Description='$b' WHERE CategoryID=".$_GET["ID"];
+			else
+				$sql = "INSERT INTO categories (CategoryName, Description)VALUES ('$a','$b')";
+				
 			if ($conn->query($sql)) {
-				echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');
+				echo "<script type= 'text/javascript'>alert('Save data Successfully');
 				      window.location.replace('index.php?pages=category');
 					 </script>";
 			}else{
-				echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+				echo "<script type= 'text/javascript'>alert('Data not successfully save data.');</script>";
 			}
 			$conn = null;
 		}
